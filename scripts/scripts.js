62f28d9e250cd453997c1f62652fb700f74bda16
Maurice Williams <morficus@gmail.com>
2014-05-21 23:00:47 -0500

    one huge massive commit... with basically everything :-(

---
+++ scripts/scripts.js
@@ -0,0 +1,24 @@
+(function(){
+
+    $('#js-go-to-form').click(function(event){
+        event.preventDefault();
+        var $target = $(event.target);
+        $(window).scrollTo('#barkground-form', 900);
+    });
+
+
+    $('#js-send-form').click(function(){
+        debugger;
+    });
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= 100){
+            $('#js-header').addClass('is-small');
+        }else{
+            $('#js-header').removeClass('is-small');
+        }
+
+    });
+
+
+})(window);
